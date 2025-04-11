8b01fbdf1213488b51b8c91d95a8dde8ebd69434
zeroxpeekky <matvejsidorenko973@gmail.com>
2025-04-11 18:36:48 +0400

    version 0.1

---
+++ temp/pingpong.py
@@ -0,0 +1,17 @@
+from pygame import *
+back = (0,255,255)
+window = display.set_mode((700,500))
+window.fill(back)
+clock = time.Clock()
+
+
+
+
+
+
+
+
+
+while True:
+    clock.tick(40)
+    display.update()
